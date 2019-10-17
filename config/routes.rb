41c51304507ecaf941aa13e4d6ce7d44eafd5e39
yukomiyama <yukomiyama@icloud.com>
2019-10-17 14:41:26 +0900

    routes callback_url changed

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
-  # get     'auth/:provider/callback', to: 'sessions#create'
-  get     '/auth/facebook/callback', to: 'sessions#create'
+  get     'auth/:provider/callback', to: 'sessions#create'
+  # get     '/auth/facebook/callback', to: 'sessions#create'
 
   resources :users do
     member do
